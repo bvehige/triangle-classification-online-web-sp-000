2e53b2d1d7db46355d25efc3ee7735b6eb7f1ec0
Brian Vehige <brianvehige@gmail.com>
2020-03-17 03:35:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,10 +10,10 @@ end
 
 def kind
   
-  if 
+  if @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
+    raise TriangleError
   
-  
-  elsif (@length1 + @length2 > @length3) || (@length1 + @length3 > @length2) || (@length2 + @length3 > @length1) || @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
+  elsif (@length1 + @length2 > @length3) || (@length1 + @length3 > @length2) || (@length2 + @length3 > @length1) 
     raise TriangleError
   
   if @length1 == @length2 && @length1 == @length3 
