c82af1ffb97d9ca2576de8e942a1c5872fafa72a
Brian Vehige <brianvehige@gmail.com>
2020-03-17 03:10:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ def kind
   if @length1 == @length2 && @length1 == @length3 
     return :equilateral
   elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3 
-    return :isoceles 
+    return :isosceles 
   elsif !(@length1 == @length2) && !(@length1 == @length2) && !(@length2 == @length3) 
     return :scalene
   end
