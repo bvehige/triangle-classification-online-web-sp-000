9ea36994e48038960b09d616433a29f4229aa4c6
Brian Vehige <brianvehige@gmail.com>
2020-03-17 03:32:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,8 @@ def initialize (length1, length2, length3)
 end
 
 def kind
-  
-  
+  if (@length1 + @length2 > @length3) || (@length1 + @length3 > @length2) || (@length2 + @length3 > @length1) || @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
+    raise TriangleError
   
   if @length1 == @length2 && @length1 == @length3 
     return :equilateral
