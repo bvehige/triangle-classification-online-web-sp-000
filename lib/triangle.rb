4eb219e9918a74efdd802a01b09f5366685cbd7f
Brian Vehige <brianvehige@gmail.com>
2020-03-17 03:21:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,9 @@ def initialize (length1, length2, length3)
 end
 
 def kind
-  if @length1 == @length2 && @length1 == @length3 
+  if (@length1 + @length2 > @length3) || (@length1 + @length3 > @length2) || (@length2 + @length3 > @length1) 
+    
+  elsif @length1 == @length2 && @length1 == @length3 
     return :equilateral
   elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3 
     return :isosceles 
