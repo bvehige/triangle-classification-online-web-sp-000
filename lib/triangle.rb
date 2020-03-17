83bdb7b4b1034abeda56864ea476f63c27f1c95e
Brian Vehige <brianvehige@gmail.com>
2020-03-17 03:28:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ def kind
   elsif !(@length1 == @length2) && !(@length1 == @length2) && !(@length2 == @length3) 
     return :scalene
   
-  else (@length1 + @length2 > @length3) || (@length1 + @length3 > @length2) || (@length2 + @length3 > @length1) || @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
+  elsif (@length1 + @length2 > @length3) || (@length1 + @length3 > @length2) || (@length2 + @length3 > @length1) || @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
     raise TriangleError
   
   end
