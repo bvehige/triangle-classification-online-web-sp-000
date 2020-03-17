2e58d16fdfa6c14fc37ce61d3fcf60378542f99b
Brian Vehige <brianvehige@gmail.com>
2020-03-17 03:28:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,18 @@ def initialize (length1, length2, length3)
 end
 
 def kind
-  if (@length1 + @length2 > @length3) || (@length1 + @length3 > @length2) || (@length2 + @length3 > @length1) || @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
-    raise TriangleError
-  elsif @length1 == @length2 && @length1 == @length3 
+  if @length1 == @length2 && @length1 == @length3 
     return :equilateral
+  
   elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3 
     return :isosceles 
+  
   elsif !(@length1 == @length2) && !(@length1 == @length2) && !(@length2 == @length3) 
     return :scalene
+  
+  else (@length1 + @length2 > @length3) || (@length1 + @length3 > @length2) || (@length2 + @length3 > @length1) || @length1 <= 0 || @length2 <= 0 || @length3 <= 0 
+    raise TriangleError
+  
   end
   
 end
